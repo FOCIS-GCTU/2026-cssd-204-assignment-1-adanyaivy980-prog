21fdb0ef61562413ac3958c9454e6e9527bca278
adanyaivy980-prog <2425404221@live.gctu.edu.gh>
2026-06-01 20:50:13 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name:IVY YAYRA ADANYA
+# STUDENT ID: 2425404221
+# Email: 2425404221@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Ivy Yayra Adanya , this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+    # Prints my initials in large stylized letters
+
+    print()
+    print("...IYA")
+    print()
+
+    print("...IIIIIIIIIIII...Y          Y...AAAAAAAAAA...****")
+    print(".....    II    ... Y        Y ...AA      AA...****")
+    print(".....    II    ...  Y      Y  ...AA      AA")
+    print(".....    II    ...   Y    Y   ...AA      AA")
+    print(".....    II    ...    Y  Y    ...AAAAAAAAAA")
+    print(".....    II    ...     YY     ...AA      AA")
+    print(".....    II    ...     YY     ...AA      AA")
+    print(".....    II    ...     YY     ...AA      AA")
+    print(".....    II    ...     YY     ...AA      AA...****")
+    print("...IIIIIIIIIIII...     YY     ...AA      AA...****")
+    print()
 
 
 main()
