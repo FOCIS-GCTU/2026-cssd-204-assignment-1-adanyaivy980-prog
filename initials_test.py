ad5eb1604e192c5841e06e42fd83e1adb7b58cef
adanyaivy980-prog <2425404221@live.gctu.edu.gh>
2026-06-01 20:40:26 +0100

    Update initials_test.py

--- initials_test.py
+++ initials_test.py
@@ -1,14 +1,34 @@
-import os.path
-import sys
-from initials import main
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name:IVY YAYRA ADANYA
+# STUDENT ID: 2425404221
+# Email: 2425404221@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Ivy Yayra Adanya , this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 
-def test_initials():
-    try:
-        exists = os.path.exists("initials.py")
-        assert exists == True
-    except:
-        sys.exit()
+def main():
+    # Prints my initials in large stylized letters
+
+    print()
+    print("...IYA")
+    print()
+
+    print("...IIIIIIIIIIII...Y          Y...AAAAAAAAAA...****")
+    print(".....    II    ... Y        Y ...AA      AA...****")
+    print(".....    II    ...  Y      Y  ...AA      AA")
+    print(".....    II    ...   Y    Y   ...AA      AA")
+    print(".....    II    ...    Y  Y    ...AAAAAAAAAA")
+    print(".....    II    ...     YY     ...AA      AA")
+    print(".....    II    ...     YY     ...AA      AA")
+    print(".....    II    ...     YY     ...AA      AA")
+    print(".....    II    ...     YY     ...AA      AA...****")
+    print("...IIIIIIIIIIII...     YY     ...AA      AA...****")
+    print()
 
 
-    main()
+main()
